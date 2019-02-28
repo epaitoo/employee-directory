1ded6a6517004082910d714badb0f7ad9269aad1
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-02-28 22:05:31 +0000

    add additional page styling

--- js/app.js
+++ js/app.js
@@ -56,7 +56,6 @@ function generateEmployees(data) {
     let searchHTML = `
         <form action="#" method="get">
             <input type="search" id="search-input" class="search-input" placeholder="Search...">
-            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
         </form>
     `
     searchDiv.innerHTML = searchHTML;
