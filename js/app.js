fb6a2aa2b7a70ae630d3915098ad6150dcb0078a
Eugene Paitoo <eugenepaitoo@gmail.com>
2019-02-28 08:56:55 +0000

    add search feature

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const searchDiv = document.querySelector('.search-container');
 const galleryDiv = document.getElementById('gallery');
 const cards = galleryDiv.getElementsByClassName('card');
 const modalContainer = document.getElementsByClassName('modal-container');
@@ -6,7 +7,7 @@ const prevButton = document.getElementsByClassName('modal-prev btn');
 const nextButton = document.getElementsByClassName('modal-next btn');
 
 
-const url = "https://randomuser.me/api/?results=12";
+const url = "https://randomuser.me/api/?results=12&nat=us,fr,gb,ca,nz";
 
 fetch(url)
     .then(response => response.json())
@@ -50,6 +51,33 @@ function generateEmployees(data) {
     ).join("");
     galleryDiv.innerHTML = employees; 
 
+
+    //search feature
+    let searchHTML = `
+        <form action="#" method="get">
+            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+            <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+        </form>
+    `
+    searchDiv.innerHTML = searchHTML;
+
+    //event listener to display search card on keyUp event
+    const searchBar = document.querySelector('#search-input');
+    searchBar.addEventListener('keyup', function(e){
+        const searchTerm = e.target.value.toLowerCase()
+        const cardInfo = document.getElementsByClassName('card-info-container');
+        let cardInfoArr = Array.from(cardInfo);
+        cardInfoArr.forEach(function(element) {
+            const cardName = element.firstElementChild.textContent;
+            if (cardName.toLowerCase().indexOf(searchTerm) != -1) {
+                element.parentElement.style.display = '';
+            } else {
+                element.parentElement.style.display = 'none';        
+            }
+        })
+    });
+
+
      //event listener to display card on click
      for (let i = 0; i < cards.length; i++) { 
         cards[i].addEventListener('click', function() {
@@ -62,53 +90,49 @@ function generateEmployees(data) {
 
     }
 
-      //event listener to close a card on card
-      for (let i = 0; i < closeButton.length; i++) {
-        closeButton[i].addEventListener('click', () => {
-               if (closeButton[i]) {
-                       modalContainer[i].style.display = 'none'; 
-               }
-           })
-       }
-
-         //event listener to show a prev card when previous button is clicked
-        for (let i = 0; i < prevButton.length; i++) {
-            prevButton[i].addEventListener('click', function(){
-                let len = modalContainer.length;
-                let prevModal = modalContainer[(i+len-1)%len];
-            
-                if (prevButton) {
+    //event listener to close a card on card
+    for (let i = 0; i < closeButton.length; i++) {
+    closeButton[i].addEventListener('click', () => {
+            if (closeButton[i]) {
                     modalContainer[i].style.display = 'none'; 
-                    prevModal.style.display = '';
-            
-                } else {
-                    prevModal.style.display = 'none';
-                }
-            })
-        }
+            }
+        })
+    }
 
-        //event listener to show next card when next button is clicked
-        for (let i = 0; i < nextButton.length; i++) {
-            nextButton[i].addEventListener('click', function() {
-                let len = modalContainer.length;
-                let prevModal = modalContainer[(i+len-1)%len];
-                let nextModal = modalContainer[(i+1)%len];
+        //event listener to show a prev card when previous button is clicked
+    for (let i = 0; i < prevButton.length; i++) {
+        prevButton[i].addEventListener('click', function(){
+            let len = modalContainer.length;
+            let prevModal = modalContainer[(i+len-1)%len];
+        
+            if (prevButton) {
+                modalContainer[i].style.display = 'none'; 
+                prevModal.style.display = '';
+        
+            } else {
+                prevModal.style.display = 'none';
+            }
+        })
+    }
 
-                if (nextButton) {
-                    modalContainer[i].style.display = 'none'; 
-                    nextModal.style.display = '';
-                    prevModal.style.display = 'none';
-                } else {
-                    nextModal.style.display = 'none';
+    //event listener to show next card when next button is clicked
+    for (let i = 0; i < nextButton.length; i++) {
+        nextButton[i].addEventListener('click', function() {
+            let len = modalContainer.length;
+            let prevModal = modalContainer[(i+len-1)%len];
+            let nextModal = modalContainer[(i+1)%len];
 
-                }
-            })
+            if (nextButton) {
+                modalContainer[i].style.display = 'none'; 
+                nextModal.style.display = '';
+                prevModal.style.display = 'none';
+            } else {
+                nextModal.style.display = 'none';
 
-        }
+            }
+        })
 
-
-
-
+    }
    
 }   
 
